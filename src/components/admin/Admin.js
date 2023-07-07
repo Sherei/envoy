43126164e8e5a6679f20207e3bba9dd66d5ffaa2
seoalirajpoot <seo.alirajpoot@gmail.com>
2023-07-07 11:28:57 +0500

    fg

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Profile from "../profile/Profile"
 import Chat from "../chatbox/Chat"
+import "./admin.css"
 
 const Admin = () => {
     return <>
-        <div className='container-fluid px-5'>
+        <div className='container-fluid admin_container'>
             <div className='row'>
                 <div className='col-lg-3 pl-4'>
                     <Profile />
